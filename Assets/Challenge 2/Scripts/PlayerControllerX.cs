0230f76360ed653e65e14bf93cffb5f9d215ab1c
Margarida-coelho <162127564+Margarida-coelho@users.noreply.github.com>
2024-03-06 00:12:14 +0000

    Challenge 2

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    bool dogSpawned;
+
+    private void Start()
+    {
+        StartCoroutine(SpawnDogs());
+    }
+    IEnumerator SpawnDogs()
+    {
+        while (!dogSpawned)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+                dogSpawned = true;
+                yield return new WaitForSeconds(.5f);
+                dogSpawned = false;
+            }
+            else
+            {
+                yield return null;
+            }
+        }
+    }
+}
