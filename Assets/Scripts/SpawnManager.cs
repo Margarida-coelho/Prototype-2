d43a31285e8275e911b1331838aef19174167a72
Margarida-coelho <162127564+Margarida-coelho@users.noreply.github.com>
2024-03-06 00:00:29 +0000

    Lesson 2.4

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,21 +8,19 @@ public class SpawnManager : MonoBehaviour
     private float spawnRangeX = 11;
     private float spawnRangeZ = 20;
 
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnAnimals", startDelay, spawnInterval);
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnAnimals()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
-            int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ);
+        int animalIndex = Random.Range(0, animalPrefabs.Length);
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
     }
 }
